f8c573142b0b7910fffdf06f97343292bcdad169
Artur Silveira <artur.oliveira.silveira@gmail.com>
2024-12-19 14:58:51 -0300

    add API endpoint to handle Get beer use case execution

--- internal/http/gin/handlers.go
+++ internal/http/gin/handlers.go
@@ -2,6 +2,7 @@ package gin
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/artursilveiradev/rest-beer/beer"
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,7 @@ import (
 // Gin handlers
 func Handlers(r *gin.Engine, service beer.UseCase) *gin.Engine {
 	r.POST("/v1/beer", storeBeer(service))
+	r.GET("/v1/beer/:id", getBeer(service))
 	return r
 }
 
@@ -31,6 +33,40 @@ func storeBeer(service beer.UseCase) gin.HandlerFunc {
 		c.JSON(http.StatusCreated, gin.H{
 			"status":  http.StatusCreated,
 			"message": "Beer stored",
+			"data": gin.H{
+				"id":    b.ID,
+				"name":  b.Name,
+				"type":  b.Type.String(),
+				"style": b.Style.String(),
+			},
+		})
+	}
+}
+
+// Get beer API handler
+func getBeer(service beer.UseCase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		parsedId, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Invalid param",
+				"error":   err.Error(),
+			})
+			return
+		}
+		b, err := service.Get(beer.ID(parsedId))
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  http.StatusNotFound,
+				"message": "Beer not found",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "Beer found",
 			"data": gin.H{
 				"id":    b.ID,
 				"name":  b.Name,
--- internal/http/gin/handlers_test.go
+++ internal/http/gin/handlers_test.go
@@ -5,6 +5,7 @@ package gin_test
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -62,5 +63,57 @@ func TestStoreBeer(t *testing.T) {
 		router.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 		assert.Equal(t, string("{\"message\":\"Internal Server Error\",\"status\":500}"), w.Body.String())
+	})
+}
+
+func TestGetBeer(t *testing.T) {
+	t.Run("StatusOK", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		s := mocks.NewMockUseCase(ctrl)
+		s.EXPECT().Get(gomock.Any()).Return(&beer.Beer{
+			ID:    beer.ID(1),
+			Name:  "Heineken",
+			Type:  beer.TypeLager,
+			Style: beer.StylePale,
+		}, nil)
+		router := gin.Default()
+		router = g.Handlers(router, s)
+		w := httptest.NewRecorder()
+		url := fmt.Sprintf("/v1/beer/%s", "1")
+		req, _ := http.NewRequest("GET", url, nil)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, string(
+			"{\"data\":{\"id\":1,\"name\":\"Heineken\",\"style\":\"Pale\",\"type\":\"Lager\"},\"message\":\"Beer found\",\"status\":200}",
+		), w.Body.String())
+	})
+
+	t.Run("StatusBadRequest", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		s := mocks.NewMockUseCase(ctrl)
+		router := gin.Default()
+		router = g.Handlers(router, s)
+		w := httptest.NewRecorder()
+		url := fmt.Sprintf("/v1/beer/%s", "foo")
+		req, _ := http.NewRequest("GET", url, nil)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, string(
+			"{\"error\":\"strconv.Atoi: parsing \\\"foo\\\": invalid syntax\",\"message\":\"Invalid param\",\"status\":400}",
+		), w.Body.String())
+	})
+
+	t.Run("StatusNotFound", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		s := mocks.NewMockUseCase(ctrl)
+		s.EXPECT().Get(gomock.Any()).Return(nil, errors.New("not found"))
+		router := gin.Default()
+		router = g.Handlers(router, s)
+		w := httptest.NewRecorder()
+		url := fmt.Sprintf("/v1/beer/%s", "1")
+		req, _ := http.NewRequest("GET", url, nil)
+		router.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusNotFound, w.Code)
+		assert.Equal(t, string("{\"message\":\"Beer not found\",\"status\":404}"), w.Body.String())
 	})
 }
